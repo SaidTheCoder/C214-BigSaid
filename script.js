32f9e5c60d0d6ca4c5ce8be1bc5cd1e9ff643421
SaidTheCoder <71198810+SaidTheCoder@users.noreply.github.com>
2022-06-19 21:03:35 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function(){
+    $("#show_chat").click(function(){
+        $(".left_window").css("display","none")
+        $(".right_window").css("display","block")
+        $(".header_back").css("display","block")
+    })
+    $("#header_back").click(function(){
+        $(".left_window").css("display","block")
+        $(".right_window").css("display","none")
+        $(".header_back").css("display","none")
+    })
+})
